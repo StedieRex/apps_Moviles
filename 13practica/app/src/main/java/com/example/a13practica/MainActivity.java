949c9a4de0566085e5d49ae6fbb64ac560f30cd8
LuisPach <luisbro123@hotmail.com>
2024-10-17 11:30:43 -0600

    actualizacion 17-10-2024 11:30

---
+++ 13practica/app/src/main/java/com/example/a13practica/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a13practica;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView web;//alt+enter si me da error
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.webView), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https:serciosdigitalesplus.com/cartas/");
+    }
+}
