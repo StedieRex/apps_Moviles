1d51d550db6aa87d5896e5c73f31e33d08b13cbb
Luis Pach <luisbro123@hotmail.com>
2024-11-27 13:01:59 -0600

    mostrar esta acabado

--- 14proyecto/app/src/main/java/com/example/a14proyecto/MostrarActivity.java
+++ 14proyecto/app/src/main/java/com/example/a14proyecto/MostrarActivity.java
@@ -1,6 +1,8 @@
 package com.example.a14proyecto;
 
 import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +19,9 @@ import java.util.ArrayList;
 public class MostrarActivity extends AppCompatActivity {
     RecyclerView rc;
     ArrayList<articulos>lista;
+    ConexionSQLiteHelper conn;
+
+    SQLiteDatabase db;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,48 @@ public class MostrarActivity extends AppCompatActivity {
         rc = (RecyclerView) findViewById(R.id.RecyclerView);
         rc.setLayoutManager(new GridLayoutManager(this,1));
 
+        /*
+        //prueba con una lista de 10 productos al azar
         for(int i=0; i<10; i++){
             articulos a = new articulos("","i ","12","11/12/2024");
             lista.add(a);
+        }/**/
+
+        try{
+            conn = new ConexionSQLiteHelper(this, "articulos", null, 3);
+            db = conn.getReadableDatabase();
+
+            String sql = "select id, nom, costo, fecha from articulo ";
+
+            System.out.println(sql);
+
+            Cursor cursor = db.rawQuery(sql,null);
+
+            String id = "";
+            String nom = "";
+            String costo = "";
+            String fecha = "";
+
+            while(cursor.moveToNext()){
+                id = ""+cursor.getInt(0);
+                nom = cursor.getString(1);
+                costo = "" + cursor.getDouble(2);
+                fecha = cursor.getString(3);
+                System.out.println("------------------------");
+                System.out.println("------------------------");
+                System.out.println("------------------------");
+                articulos a = new articulos(id,nom,costo,fecha);
+                lista.add(a);
+                System.out.println("id: "+id+ " nom: "+nom+" costo: "+costo);
+
+            }
+
+            cursor.close();
+            db.close();
+        }catch (Exception ex){
+            System.out.println("------------------------");
+            System.out.println(ex.getMessage().toString());
+            System.out.println("------------------------");
         }
 
         adaptar ad = new adaptar(lista);
