71026fa789ba6d4d1bfae105b2cde3e7c4914b97
Luis Pach <luisbro123@hotmail.com>
2024-11-27 00:11:01 -0600

    se sigue trabajando con mostrando

--- 14proyecto/app/src/main/java/com/example/a14proyecto/MostrarActivity.java
+++ 14proyecto/app/src/main/java/com/example/a14proyecto/MostrarActivity.java
@@ -1,5 +1,6 @@
 package com.example.a14proyecto;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,9 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MostrarActivity extends AppCompatActivity {
-
+    RecyclerView rc;
+    ArrayList<articulos>lista;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,17 @@ public class MostrarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lista = new ArrayList<articulos>();
+        rc = (RecyclerView) findViewById(R.id.RecyclerView);
+        rc.setLayoutManager(new GridLayoutManager(this,1));
+
+        for(int i=0; i<10; i++){
+            articulos a = new articulos("","i ","12","11/12/2024");
+            lista.add(a);
+        }
+
+        adaptar ad = new adaptar(lista);
+        rc.setAdapter(ad);
     }
 }
